6c7ee6d98465cc688efe1f99623321332c4290d4
shadow <shadow@example.com>
2026-06-11 03:05:50 +0800

    Keep plugin updates on upstream

--- tests/plugin-download-resolution.tests.ps1
+++ tests/plugin-download-resolution.tests.ps1
@@ -26,8 +26,8 @@ Assert-PluginContains 'if \[ -z "\$LAT_PACKAGE" \]' `
 Assert-PluginContains "Can't find NCT6687d Drivers" `
   'Plugin should print a specific error when no release asset can be found.'
 
-Assert-PluginContains '<!ENTITY pluginURL "https://github.com/shadowxhero/unraid-&name;/raw/master/&name;.plg">' `
-  'Plugin update URL should point to the shadowxhero fork.'
+Assert-PluginContains '<!ENTITY pluginURL "&gitURL;/&name;.plg">' `
+  'Plugin update URL should continue to point to the upstream plugin.'
 
 Assert-PluginContains 'DL_URL="https://github.com/ich777/unraid-nct6687-driver/releases/download/\$KERNEL_V"' `
   'Driver package downloads should continue to use ich777 release assets.'
